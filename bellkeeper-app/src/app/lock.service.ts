a372de225e3390953c534f52a913924df67bf9aa
maxdup <mdupuis@hotmail.ca>
2017-03-06 11:43:27 -0500

    stylesheet updates

--- bellkeeper-app/src/app/lock.service.ts
+++ bellkeeper-app/src/app/lock.service.ts
@@ -1,6 +1,5 @@
-import { Http, Response, URLSearchParams } from '@angular/http';
+import { Http, URLSearchParams } from '@angular/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/timeout';
